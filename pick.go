ea18053947412f3ba96548fb381dd6803f66be30
Michael Bruce <me@michaelbruce.co>
2019-03-04 20:50:32 +0000

    first passing test

--- pick.go
+++ pick.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+func Filter(col []string, filter string) []string {
+	return []string{
+		"bar",
+		"broken",
+	}
+}
+
 func main() {
 	fmt.Println("hello")
 }
--- pick_test.go
+++ pick_test.go
@@ -1,10 +1,37 @@
-package main_test
+package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestTesting(t *testing.T) {
-	fmt.Println("ok")
+func compareSlice(a, b []string) bool {
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+// I want to be able to filter a list and only keep elements beginning with b.
+func TestFindsAllStringsContainingB(t *testing.T) {
+	listOfFiles := []string{
+		"thing",
+		"other",
+		"things",
+		"bar",
+		"foo",
+		"broken",
+	}
+
+	result := Filter(listOfFiles, "b")
+
+	expectedResult := []string{
+		"bar",
+		"broken",
+	}
+
+	if !compareSlice(result, expectedResult) {
+		t.Errorf("did not get the list of expected files back")
+	}
 }
