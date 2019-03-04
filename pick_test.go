f00160b5c1830cbaea33c856defaf764cb4634ae
Michael Bruce <me@michaelbruce.co>
2019-03-04 21:08:06 +0000

    improve slice comparison for testing

--- pick_test.go
+++ pick_test.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
 func compareSlice(a, b []string) bool {
+	if len(a) == 0 || len(b) == 0 {
+		return false
+	}
 	for i := range a {
 		if a[i] != b[i] {
 			return false
@@ -13,17 +17,17 @@ func compareSlice(a, b []string) bool {
 	return true
 }
 
-// I want to be able to filter a list and only keep elements beginning with b.
-func TestFindsAllStringsContainingB(t *testing.T) {
-	listOfFiles := []string{
-		"thing",
-		"other",
-		"things",
-		"bar",
-		"foo",
-		"broken",
-	}
+var listOfFiles = []string{
+	"thing",
+	"other",
+	"things",
+	"bar",
+	"foo",
+	"broken",
+}
 
+// I want to be able to filter a list and only keep elements beginning with b.
+func TestFindsSequentialSearches(t *testing.T) {
 	resultB := Filter(listOfFiles, "b")
 
 	expectedResultForB := []string{
@@ -45,5 +49,16 @@ func TestFindsAllStringsContainingB(t *testing.T) {
 
 	if !compareSlice(resultT, expectedResultForT) {
 		t.Errorf("did not get the list of expected files back for filter t, got: %v", resultT)
+	}
+}
+
+func TestFindsFuzzySearches(t *testing.T) {
+	resultTS := Filter(listOfFiles, "ts")
+	fmt.Println(resultTS)
+
+	expectedResultForTS := []string{"things"}
+
+	if !compareSlice(resultTS, expectedResultForTS) {
+		t.Errorf("did not get the list of expected files back for filter ts, got: %v", resultTS)
 	}
 }
