4b0c55a48b594b1e6b83a574e476e75a055e87ce
Michael Bruce <me@michaelbruce.co>
2019-03-05 20:25:13 +0000

    unbuffered input and reprinting filtered results

--- pick.go
+++ pick.go
@@ -3,9 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"os/signal"
 	"regexp"
 	"strings"
+	"syscall"
 )
 
 func Filter(col []string, filter string) []string {
@@ -33,10 +37,6 @@ func readStdin() []string {
 	return list
 }
 
-// let's own 20 rows of the terminal
-// 20th row contains: "80/360 > [current search]"
-// remaining 11 rows have top matches
-// one match is selected as current
 func printInterface(list []string, w *bufio.Writer) {
 	for i, e := range list {
 		if i == 20 {
@@ -60,16 +60,44 @@ func main() {
 	listIn := readStdin()
 	printInterface(listIn, ttyw)
 
-	ttyr := bufio.NewScanner(tty)
+	// ttyr := bufio.NewScanner(tty)
+	ttyr := io.Reader(tty)
 
-	var result string
-	for ttyr.Scan() {
-		filterResults := Filter(listIn, ttyr.Text())
-		ttyw.WriteString(strings.Join(filterResults, " ") + "\n")
+	// disable input buffering
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	// do not display entered characters on the screen
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+	// restore the echoing state when exiting
+	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
 
-		result = filterResults[0]
-		break
+	// ensure we use echo even on sigterm/ctrl+c
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		exec.Command("stty", "-F", "/dev/tty", "echo").Run()
+		os.Exit(1)
+	}()
+
+	var userSearch string
+
+	var b []byte = make([]byte, 1)
+	for {
+		ttyr.Read(b)
+		userSearch = userSearch + string(b)
+		printInterface(Filter(listIn, userSearch), ttyw)
+		ttyw.WriteString(userSearch)
+		ttyw.Flush()
 	}
 
-	fmt.Printf(result)
+	// var result string
+	// for ttyr.Scan() {
+	// 	filterResults := Filter(listIn, ttyr.Text())
+	// 	ttyw.WriteString(strings.Join(filterResults, " ") + "\n")
+
+	// 	result = filterResults[0]
+	// 	break
+	// }
+
+	// fmt.Printf(result)
 }
